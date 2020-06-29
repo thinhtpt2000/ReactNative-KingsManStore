8366ad7aac9963032aa870422133d1b07e343f2c
thinhtpt2000 <tthinh2128@gmail.com>
2020-06-29 23:42:10 +0700

    add more category item

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
+// Import Images Category
+import SuitImage from './assets/suit.png';
+import TieImage from './assets/tie.png';
+import SunGlassImage from './assets/sun-glasses.png';
+import WatchImage from './assets/watch.png';
+import TrousersImage from './assets/trousers.png';
+import CaneImage from './assets/cane.png';
+import UmbrellaImage from './assets/umbrella.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CategoryListItem />
+      <CategoryListItem images={SuitImage} title='Suit Items'/>
+      <CategoryListItem images={TrousersImage} title='Trousers Items'/>
+      <CategoryListItem images={TieImage} title='Tie Items'/>
+      <CategoryListItem images={SunGlassImage} title='Sun Glasses Items'/>
+      <CategoryListItem images={WatchImage} title='Watch Items'/>
+      <CategoryListItem images={CaneImage} title='Cane Items'/>
+      <CategoryListItem images={UmbrellaImage} title='Umbrella Items'/>
     </View>
   );
 }
@@ -15,7 +29,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingLeft: 16,
+    paddingRight: 16
   },
 });
--- components/CategoryListItem.js
+++ components/CategoryListItem.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import {
     Image,
+    StyleSheet,
     Text,
     View
 } from 'react-native';
-import SuitImage from '../assets/suit.png';
 
 
 export default function CategoryListItem(props) {
-    return <View>
-        <Text>CategoryListItem</Text>
-        <Image source={SuitImage} />
+    return <View style={styles.container}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Image style={styles.categoryImage} source={props.images} />
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        padding: 16,
+        borderRadius: 4,
+        backgroundColor: '#FFF',
+        shadowColor: '#00F',
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 0 },
+        elevation: 1, // use to see shadow in Android,
+        marginBottom: 16
+    },
+    title: {
+        textTransform: 'uppercase',
+        marginBottom: 8,
+        fontWeight: '700'
+    },
+    categoryImage: {
+        width: 64,
+        height: 64
+    }
+});
