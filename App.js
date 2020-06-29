fc7cee47094bc9030c89d07fd09a33847634c800
thinhtpt2000 <tthinh2128@gmail.com>
2020-06-29 23:47:23 +0700

    fix overflow category items

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoryListItem from './components/CategoryListItem';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import CategoryListItem from "./components/CategoryListItem";
 // Import Images Category
-import SuitImage from './assets/suit.png';
-import TieImage from './assets/tie.png';
-import SunGlassImage from './assets/sun-glasses.png';
-import WatchImage from './assets/watch.png';
-import TrousersImage from './assets/trousers.png';
-import CaneImage from './assets/cane.png';
-import UmbrellaImage from './assets/umbrella.png';
+import SuitImage from "./assets/suit.png";
+import TieImage from "./assets/tie.png";
+import SunGlassImage from "./assets/sun-glasses.png";
+import WatchImage from "./assets/watch.png";
+import TrousersImage from "./assets/trousers.png";
+import CaneImage from "./assets/cane.png";
+import UmbrellaImage from "./assets/umbrella.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CategoryListItem images={SuitImage} title='Suit Items'/>
-      <CategoryListItem images={TrousersImage} title='Trousers Items'/>
-      <CategoryListItem images={TieImage} title='Tie Items'/>
-      <CategoryListItem images={SunGlassImage} title='Sun Glasses Items'/>
-      <CategoryListItem images={WatchImage} title='Watch Items'/>
-      <CategoryListItem images={CaneImage} title='Cane Items'/>
-      <CategoryListItem images={UmbrellaImage} title='Umbrella Items'/>
+      <ScrollView style={styles.scrollView}>
+        <CategoryListItem images={SuitImage} title="Suit Items" />
+        <CategoryListItem images={TrousersImage} title="Trousers Items" />
+        <CategoryListItem images={TieImage} title="Tie Items" />
+        <CategoryListItem images={SunGlassImage} title="Sun Glasses Items" />
+        <CategoryListItem images={WatchImage} title="Watch Items" />
+        <CategoryListItem images={CaneImage} title="Cane Items" />
+        <CategoryListItem images={UmbrellaImage} title="Umbrella Items" />
+      </ScrollView>
     </View>
   );
 }
@@ -28,10 +30,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     // alignItems: 'center',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    alignItems: "stretch",
+    justifyContent: "center",
+    // paddingLeft: 16,
+    // paddingRight: 16,
+  },
+  scrollView: {
     paddingLeft: 16,
     paddingRight: 16
   },
