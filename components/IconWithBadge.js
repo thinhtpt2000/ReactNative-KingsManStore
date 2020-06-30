a215e2ed7cf2ce31b79f4962d2c3810c4a59204b
thinhtpt2000 <tthinh2128@gmail.com>
2020-06-30 23:26:09 +0700

    fix over-number

--- components/IconWithBadge.js
+++ components/IconWithBadge.js
@@ -3,12 +3,12 @@ import {
   Text,
   View,
 } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/AntDesign";
 
 const IconWithBadge = ({ name, badgeCount, color, size }) => {
   return (
     <View style={{ width: 24, height: 24, margin: 5 }}>
-      <Ionicons name={name} size={size} color={color} />
+      <Icon name={name} size={size} color={color} />
       {badgeCount > 0 && (
         <View
           style={{
@@ -17,9 +17,9 @@ const IconWithBadge = ({ name, badgeCount, color, size }) => {
             right: -6,
             top: -3,
             backgroundColor: "red",
-            borderRadius: 6,
-            width: 12,
-            height: 12,
+            borderRadius: 18,
+            width: 18,
+            height: 18,
             justifyContent: "center",
             alignItems: "center",
           }}
