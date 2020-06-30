d07f81d47863a65f891cca75e15cd72abcede5ef
thinhtpt2000 <tthinh2128@gmail.com>
2020-06-30 23:27:53 +0700

    add CartContext

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { FlatList, View, StyleSheet } from "react-native";
+import CartListItem from "../components/CartListItem";
+import CartTotal from "../components/CartTotal";
 
-const CartSreen = props => {
-    return (<Text>Cart</Text>);
-}
+import { CartContext } from "../contexts/Cart";
+
+const CartSreen = (props) => {
+  //   const getOptimizeCartItems = (cartItems) => {
+  //     let tmpCartItems = {};
+  //     for (let item of cartItems) {
+  //       if (tmpCartItems[item.id] === undefined) {
+  //           item.quantity = 1;
+  //           tmpCartItems[item.id] = item;
+  //       }
+  //       else {
+  //           tmpCartItems[item.id].quantity += 1;
+  //       }
+  //     }
+  //     cartItems = [];
+  //     for (let item in tmpCartItems) {
+  //         cartItems.push(tmpCartItems[item]);
+  //     }
+  //     return cartItems;
+  //   };
+
+  return (
+    <View style={styles.container}>
+      <CartContext.Consumer>
+        {({ cartItems }) => {
+          // const optimizeCartItems = getOptimizeCartItems(cartItems);
+          return (
+            <FlatList
+              data={cartItems}
+              renderItem={({ item }) => <CartListItem cartItem={item} />}
+              keyExtractor={(item) => `${item.id}`}
+              style={{paddingHorizontal: 8}}
+            />
+          );
+        }}
+      </CartContext.Consumer>
+      <CartTotal style={styles.total} />
+    </View>
+  );
+};
 
 CartSreen.navigationOptions = {
-    title: "Cart",
+  title: "Cart",
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    marginTop: 16,
+  },
+  total: {
+    justifyContent: "flex-end",
+  },
+});
 
 export default CartSreen;
